df3ae5d9e706ac38fe518e9a1a6c7a7c29608df6
rashi-101 <rashichawla2608@gmail.com>
2021-05-02 14:56:31 +0530

    menuBar-formulaBar created with working align btns

--- 5.Browser/activity/excel_clone/script.js
+++ 5.Browser/activity/excel_clone/script.js
@@ -1,6 +1,7 @@
 let plus_container = document.querySelector(".plus-container");
 let sheet_list = document.querySelector(".sheet-list");
 
+
 plus_container.addEventListener("click", function(){
     let sheetsArr = document.querySelectorAll(".sheet");
     let lastSheet = sheetsArr[sheetsArr.length-1];
@@ -42,8 +43,61 @@ str="";
 for(let i=0; i<100; i++){
     str+='<div class="row">'
     for(let j=0; j<26; j++){
-        str+=`<div class="col">${String.fromCharCode(65+j)}${i+1}</div>`;
+        str+=`<div class="col" rid="${i}" cid ="${j}" contenteditable="true"></div>`;
     }
     str+="</div>"
 }
 grid.innerHTML=str;
+
+
+let allCells = document.querySelectorAll(".grid .col");
+let addressBar = document.querySelector(".address-box");
+for(let i=0; i<allCells.length;i++){
+    allCells[i].addEventListener("click", function(){
+        let rid = Number(allCells[i].getAttribute("rid"))+1;
+        let cid = Number(allCells[i].getAttribute("cid"));
+        let colAdd = String.fromCharCode(cid + 65);
+        let address = colAdd+rid;
+        addressBar.value=address;
+    });
+}
+allCells[0].click();
+
+let leftBtn = document.querySelector(".left");
+let centerBtn = document.querySelector(".center");
+let rightBtn = document.querySelector(".right");
+
+leftBtn.addEventListener("click", function(){
+    let address = addressBar.value;
+    let { rid, cid} = getRCidFromAdress(address);
+    console.log(rid +" "+ cid);
+    let cell = document.querySelector(`.col[rid="${rid}"][cid="${cid}"]`);
+    console.log(cell);
+    cell.style.textAlign = "left";
+});
+
+
+rightBtn.addEventListener("click", function(){
+    let address = addressBar.value;
+    let { rid, cid} = getRCidFromAdress(address);
+    console.log(rid +" "+ cid);
+    let cell = document.querySelector(`.col[rid="${rid}"][cid="${cid}"]`);
+    console.log(cell);
+    cell.style.textAlign = "right";
+});
+centerBtn.addEventListener("click", function(){
+    let address = addressBar.value;
+    let { rid, cid} = getRCidFromAdress(address);
+    console.log(rid +" "+ cid);
+    let cell = document.querySelector(`.col[rid="${rid}"][cid="${cid}"]`);
+    console.log(cell);
+    cell.style.textAlign = "center";
+});
+
+function getRCidFromAdress(address){
+    let cellColAdr = address.charCodeAt(0);
+    let cellRowAdr = address.slice(1);
+    let cid = cellColAdr-65;
+    let rid = Number(cellRowAdr)-1 ;
+    return {rid, cid};
+}
